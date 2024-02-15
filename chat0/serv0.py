406922322015760f0a600e42c2b0cc1376866b72
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-02-15 15:31:37 +0700

    .

---
+++ chat0/serv0.py
@@ -0,0 +1,38 @@
+import socket
+import time
+
+
+host = socket.gethostbyname(socket.gethostname())
+port = 9090
+
+clients = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+
+quit = False
+print('-----server started-----')
+(host): print("[ Server Started IP: "+host+" ]")
+
+while not quit:
+	try:
+		data, addr = s.recvfrom(1024)
+
+		if addr not in clients:
+			clients.append(addr)
+
+		itsatime = time.strftime('%Y-%m-%d-%H.%M.%S', time.localtime())
+
+		print('['+addr[0]+']=['+str(addr[1])+']=['+itsatime+']/',
+		      end='')
+		print(data.decode('utf-8'))
+
+		for client in clients:
+			if addr != client:
+				s.sendto(data,client)
+
+	except:
+		print('\n-----server stopped-----')
+		quit = True
+
+s.close()
