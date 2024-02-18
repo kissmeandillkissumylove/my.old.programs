d8e78f423280cb414b5749f3fc410f0e69878540
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-02-18 18:08:38 +0700

    .

---
+++ game1/settings.py
@@ -0,0 +1,22 @@
+class Settings():
+	"""Класс для хранения всех настроек игры Alien Invasion."""
+	
+	def __init__(self):
+		"""Инициализирует настройки игры."""
+		# Параметры экрана
+		self.screen_width = 1200
+		self.screen_height = 600
+		self.bg_color = (200, 200, 110)
+		# Настройки корабля
+		self.ship_speed = 1.5
+		# Параметры снаряда
+		self.bullet_speed = 3
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 60, 60)
+		self.bullets_allowed = 3
+		# Настройки пришельцев
+		self.alien_speed = 1.0
+		self.fleet_drop_speed = 10
+		# fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+		self.fleet_direction = 1
