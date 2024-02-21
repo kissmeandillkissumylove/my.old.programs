683eede59c904ebbb78d08fbbfda07a909b23b29
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-02-21 14:43:23 +0700

    .

---
+++ game368/level.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+
+class Level:
+
+	def __init__(self):
+		# sprite group setup
+		self.visible_sprites = pygame.sprite.Group()
+		self.obstacles_sprites = pygame.sprite.Group()
+
+	def run(self):
+		#upgrade and draw the game
+		pass
