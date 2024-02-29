ed9b3860d01dc9ea0a33b38d777ba3faeeeff902
kissmeandillkissumylove <121801294+kissmeandillkissumylove@users.noreply.github.com>
2024-02-29 21:08:49 +0700

    .

---
+++ work_with_os/main.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+
+def main():
+    print()
+    # print(os.getcwd())
+    # print(os.name)
+    # print(os.listdir(os.getcwd()))
+    # print(os.chdir('C:\\kissmeandillkissumylove\\0003.python-work\\'))
+    # print([print(elt) for elt in os.listdir(os.getcwd())])
+
+    # print(os.listdir())
+    # os.mkdir('example_create_dir')
+    # print(os.listdir())
+
+    # os.makedirs("example/example1/example2")
+
+    # os.removedirs('example')
+    # os.removedirs("example/example1/example2")
+    # os.removedirs('example_create_dir')
+
+    # os.mkdir('ex\ex1')
+
+    # print(sys.getfilesystemencoding())
+
+    # print(os.ctermid()) # for Unix
+
+    # print(os.getlogin())
+
+    # print(os.getpid()) # Return the current process id.
+
+    
+    return 0
+
+if __name__ == '__main__':
+    main()
